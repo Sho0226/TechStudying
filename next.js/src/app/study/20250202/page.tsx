90f90ff8b8b2a7074fe1cc95b83b665ac84edfb7
sho0226 <s1f102201616@iniad.org>
2025-02-02 20:58:19 +0900

    tailwindのサイズ

---
+++ next.js/src/app/study/20250202/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <div className="w-8 bg-white">32px</div>
+      <div className="w-full bg-white">100%</div>
+      <div className="max-w-ws bg-white">Text</div>
+      <div className="h-screen bg-white">Text</div>
+    </div>
+  );
+}
+
+export default page;
--- next.js/src/utils/days.ts
+++ next.js/src/utils/days.ts
@@ -1,4 +1,5 @@
 export const studyDays = [
   { date: "20250131", title: "2025-01-31: TechStydying in Next.js Started" },
+  { date: "20250202", title: "2025-02-02: Tailwind CSS Size" },
   // 必要に応じて日付とタイトルを追加
 ];
